2603a4bea9a92417a298bf90270901e262dfffcf
hailyn <hailynfc99@gmail.com>
2023-10-15 17:29:18 -0300

    Initial commit

---
+++ app/src/main/java/com/example/actividad2_2/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.actividad2_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imagen;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imagen= findViewById(R.id.imagen);
+    }
+
+    public void sendImage(View v) {
+                new Thread(new Runnable() {
+            public void run() {
+                final Bitmap bitmap = loadImageFromNetwork("https://vetplus.vet.br/wp-content/uploads/2019/12/meme-da-mulher-gritando-com-o-gato-na-mesa-og-1080x630.jpg");
+
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        imagen.setImageBitmap(bitmap);
+                    }
+                });
+            }
+        }).start();
+    }
+
+
+
+    public Bitmap loadImageFromNetwork(String imageUrl) {
+        try {
+            URL url = new URL(imageUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            return BitmapFactory.decodeStream(input);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+
+
+
+}
